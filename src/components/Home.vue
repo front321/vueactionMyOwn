a7260192be13cd2fc8f96d5980c58081ee28a68b
ouyang <ouyang@test.com>
2023-04-05 21:10:00 +0800

    初步改造

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>hello</h1>
+    <el-button>hello</el-button>
+    <el-button type="primary">hello</el-button>
+    <el-button type="info">hello</el-button>
+    <el-button type="danger">hello</el-button>
+    <el-button type="success">hello</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
