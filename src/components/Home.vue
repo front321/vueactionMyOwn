ff069844d41370187d2ab26762dce5134b016a8c
ouyang <ouyang@test.com>
2023-04-08 23:27:14 +0800

    style的lang统一为scss

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less">
+<style lang="scss">
     .hello {
       background: yellow;
       .el-button {
