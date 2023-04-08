7f540cb4c76a60ce755d4f54b1ba50e893d08451
ouyang <ouyang@test.com>
2023-04-08 23:04:39 +0800

    使用scss

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,6 +20,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss">
+    .hello {
+      background: yellow;
+      .el-button {
+        color: red
+      }
+    }
 </style>
