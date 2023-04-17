3aadc4a6b8f94b7c892cdcd8bd45373e9d0766bd
ouyang <ouyang@test.com>
2023-04-17 22:22:38 +0800

    路由简单使用

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import './plugins/element.js'
 //导入字体库的css
 import 'font-awesome/css/font-awesome.min.css'
 import axios from 'axios'
+import router from './router'
 
 Vue.prototype.axios = axios //挂载到原型，可以在全局使用
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
 
