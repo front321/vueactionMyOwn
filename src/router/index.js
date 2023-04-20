7d27f9102534afae2140b7aca5c9c7ec93c884b9
ouyang <ouyang@test.com>
2023-04-20 22:01:01 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/Home.vue'
+//import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
@@ -8,7 +8,8 @@ export default new Router({
     routes: [
         {
             path: '/home',
-            component: Home
+            //component: Home
+            component: () => import('@/components/Home') //路由懒加载
         }
     ],
     mode: 'history'
