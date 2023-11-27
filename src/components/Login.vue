8c62a6350bdc9fa2e4b9dba51394eb33c6fbe9f0
ouyang <ouyang@test.com>
2023-11-27 16:56:18 +0800

    点击登录时为用户名和密码添加校验

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,16 +9,25 @@
             label-width="80px"
             :model="form"
             ref="form">
-                <el-form-item label="用户名" prop="username">
+                <el-form-item label="用户名" prop="username"
+                :rules="[
+                {required: true, message: '请输入用户名', trigger: 'blur'},
+                {min: 6, max: 12, message: '长度在6-12位字符', trigger: 'blur'}
+                ]">
                     <el-input v-model="form.username"></el-input>
                 </el-form-item>
 
-                <el-form-item label="密码" prop="password">
+                <el-form-item label="密码" prop="password"
+                :rules="[
+                {required: true, message: '请输入密码', trigger: 'blur'},
+                {min: 6, max: 12, message: '长度在6-12位字符', trigger: 'blur'}
+                ]"
+                >
                     <el-input type="password" v-model="form.password"></el-input>
                 </el-form-item>
 
                 <el-form-item>
-                    <el-button type="primary">登录</el-button>
+                    <el-button type="primary" @click="login()">登录</el-button>
                 </el-form-item>
             </el-form>
 
@@ -34,6 +43,17 @@ export default {
                 username: '',
                 password: ''
             }
+        }
+    },
+    methods: {
+        login() {
+            this.$refs["form"].validate((valid) => {
+                if (valid) {
+                    console.log(this.form)
+                } else {
+                    console.error(this.form)
+                }
+            })
         }
     }
 }
