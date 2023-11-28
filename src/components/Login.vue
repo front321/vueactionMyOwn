474573c3fd9573ff887fb4d269899144531f1b41
ouyang <ouyang@test.com>
2023-11-28 17:34:21 +0800

    校验方法写在data()函数中，注意此时不能再用this引用他们

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -31,6 +31,29 @@
 <script>
 export default {
     data() {
+        const validateUsername = (rule, value, callback) => {
+            //用户名由字母（大小写）、数字、下划线和短横线组成，长度在4到16个字符之间
+            const pattern = /^[a-zA-Z0-9_-]{4,16}$/;
+            if (value === "") {
+                callback(new Error("请输入用户名"));
+            } else if (!pattern.test(value)){
+                callback(new Error("用户名由字母（大小写）、数字、下划线和短横线组成，长度在4到16个字符之间"));
+            } else {
+                callback();
+            }
+        };
+
+        const validatePassword = (rule, value, callback) => {
+            //密码至少包含一个小写字母、一个大写字母和一个数字，长度至少为8个字符
+            const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+            if (value === "") {
+                callback(new Error("请输入密码"));
+            } else if (!pattern.test(value)) {
+                callback(new Error("密码至少包含一个小写字母、一个大写字母和一个数字，长度至少为8个字符"));
+            } else {
+                callback();
+            }
+        };
         return {
             form: {
                 username: '',
@@ -38,37 +61,37 @@ export default {
             },
             rules: {
                 username: [
-                    {required: true, message: '请输入用户名', trigger: 'blur'},
-                    {validator: this.validateUsername, trigger: 'blur'}
+                    // {required: true, message: '请输入用户名11', trigger: 'blur'},
+                    {validator: validateUsername, trigger: 'blur'}
                 ],
                 password: [
-                    {required: true, message: '请输入密码eee', trigger: 'blur'},
-                    {validator: this.validatePassword, trigger: 'blur'}
+                    // {required: true, message: '请输入密码11', trigger: 'blur'},
+                    {validator: validatePassword, trigger: 'blur'}
                 ]
             }
         }
     },
     methods: {
-        //校验用户名
-        validateUsername(rule, value, callback) {
-            //用户名由字母（大小写）、数字、下划线和短横线组成，长度在4到16个字符之间
-            const pattern = /^[a-zA-Z0-9_-]{4,16}$/;
-            if (!pattern.test(value)) {
-                callback(new Error('用户名格式不正确'));
-            } else {
-                callback();
-            }
-        },
-        //校验密码
-        validatePassword(rule, value, callback) {
-            //密码至少包含一个小写字母、一个大写字母和一个数字，长度至少为8个字符
-            const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-            if (!pattern.test(value)) {
-                callback(new Error('密码格式不正确'));
-                } else {
-                    callback();
-                }
-            },
+        // //校验用户名
+        // validateUsername(rule, value, callback) {
+        //     //用户名由字母（大小写）、数字、下划线和短横线组成，长度在4到16个字符之间
+        //     const pattern = /^[a-zA-Z0-9_-]{4,16}$/;
+        //     if (!pattern.test(value)) {
+        //         callback(new Error('用户名格式不正确'));
+        //     } else {
+        //         callback();
+        //     }
+        // },
+        // //校验密码
+        // validatePassword(rule, value, callback) {
+        //     //密码至少包含一个小写字母、一个大写字母和一个数字，长度至少为8个字符
+        //     const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        //     if (!pattern.test(value)) {
+        //         callback(new Error('密码格式不正确'));
+        //         } else {
+        //             callback();
+        //         }
+        // },
 
         login() {
             this.$refs["form"].validate((valid) => {
